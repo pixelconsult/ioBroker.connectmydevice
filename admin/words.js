d1fe84e14de09a7621e4737a3643c2a260b35a98
Andreas Stieger <ast@pixelconsult.at>
2020-05-29 14:54:31 +0200

    :memo: inital release

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'connectmydevice adapter settings': {
+		'en': 'Adapter settings for connectmydevice',
+		'de': 'Adaptereinstellungen für connectmydevice',
+		'ru': 'Настройки адаптера для connectmydevice',
+		'pt': 'Configurações do adaptador para connectmydevice',
+		'nl': 'Adapterinstellingen voor connectmydevice',
+		'fr': "Paramètres d'adaptateur pour connectmydevice",
+		'it': "Impostazioni dell'adattatore per connectmydevice",
+		'es': 'Ajustes del adaptador para connectmydevice',
+		'pl': 'Ustawienia adaptera dla connectmydevice',
+		'zh-cn': 'connectmydevice的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
